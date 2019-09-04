fcf169295700ec0f746071c8d5c834dec9a3e569
latcho.nikolov@gmail.com <niceslacker77>
2019-09-04 10:27:08 +0300

    lifting state up

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
 import "./App.css";
 import Details from "./components/Details";
+import Menu from "./components/Menu";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [],
+      category: "JavaScript"
+    };
+  }
+
+  handleSearch = text => {
+    this.setState({ category: text });
+    this.makeFetch(text);
+  };
+
+  componentDidMount() {
+    this.makeFetch();
+  }
+
+  makeFetch() {
+    const category = this.state.category;
+    console.log("fetching " + category);
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${category}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonResponse => this.setState({ books: jsonResponse.items }));
+  }
+
   render() {
+    const books = this.state.books;
     return (
       <div className="container">
-        <div>Hello World!</div>
-        <Details />
+        <Menu search={this.handleSearch} />
+        <Details books={books} />
       </div>
     );
   }
